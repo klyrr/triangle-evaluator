c26a2278d9c45a9cf7e48a9096200706c55dd73f
Tina Umlandt <tina.umlandt@gmail.com>
2017-02-26 17:29:51 +0100

    feat: Add exec mvn target to execute program from command line

---
+++ src/main/java/com/klyrr/tradeshift/Main.java
@@ -0,0 +1,24 @@
+package com.klyrr.tradeshift;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(final String[] args) {
+
+        final Scanner in = new Scanner(System.in);
+        System.out.println("Please enter the length of the three triangle sides:");
+
+
+        System.out.println("side A");
+        final int sideA = in.nextInt();
+
+        System.out.println("side B");
+        final int sideB = in.nextInt();
+
+        System.out.println("side C");
+        final int sideC = in.nextInt();
+
+        System.out.println(String.format("Your triangle is %s", TriangleType.SCALENE));
+    }
+}
